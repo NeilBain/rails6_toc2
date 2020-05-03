a67a7512653e18a9b92a4a0d09b34dedf236bfb1
NeilBain <git@realrails.co.uk>
2020-05-03 20:05:51 +0100

    Populated models

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,20 @@
 class Task < ApplicationRecord
+  belongs_to :project
+  
+    validates :av_duration, numericality: {less_than_or_equal_to: :max_duration}, allow_nil: true 
+    validates :max_duration, numericality: {greater_than_or_equal_to: :av_duration}, allow_nil: true
+  
+    before_save :calculate_toc_factor
+    after_save :calculate_project_buffer
+    after_destroy :calculate_project_buffer
+  
+  
+    def calculate_toc_factor
+      self.toc_factor = ((self.max_duration - self.av_duration)/2)**2
+    end
+  
+    def calculate_project_buffer
+      @project = Project.find(self.project_id)
+      @project.update_attributes(min_duration: 1)
+    end
 end
