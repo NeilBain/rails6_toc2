a6e036f9e4fd6890ec7fcbf8f9fec2a202e7c085
NeilBain <git@realrails.co.uk>
2020-05-04 12:54:17 +0000

    Adds controllers and routes closes #2 closed #3 closes #5

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'static/home'
+  get 'static/about'
+  get 'static/help'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+	resources :projects do
+		resources :tasks
+	end
+
+	root to: 'static#home'
 end
